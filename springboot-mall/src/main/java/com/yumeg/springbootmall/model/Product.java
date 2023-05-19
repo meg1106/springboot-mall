6fd0ba7390f693ce3570ac893ed0c00c91e0e38f
meg <meg@wethink.com.tw>
2023-05-19 17:51:49 +0800

    商品功能：設定返回的時間格式＆時區

--- springboot-mall/src/main/java/com/yumeg/springbootmall/model/Product.java
+++ springboot-mall/src/main/java/com/yumeg/springbootmall/model/Product.java
@@ -1,5 +1,6 @@
 package com.yumeg.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yumeg.springbootmall.constant.ProductCategory;
 import lombok.Data;
 
@@ -14,6 +15,12 @@ public class Product {
     private Integer price;
     private Integer stock;
     private String description;
+
+    // Date 類型預設使用英國格林威治時區（ＧＭＴ＋0）
+    // 在application.properties中設定GMT+8即可更改時間格式
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private Date createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
     private Date lastModifiedDate;
+
 }
